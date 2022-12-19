25a1fbcd903144e5a6275b699b3cc964a3003461
Elekwachi Champion <117746523+ECJChampion@users.noreply.github.com>
2022-12-19 16:28:37 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,9 @@
-void reset_to_98(int *n) {
-  *n = 98;
+/**
+
+reset_to_98 - function that updates the value of a given int to 98
+@n: pointer to int to be updated
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
 }
