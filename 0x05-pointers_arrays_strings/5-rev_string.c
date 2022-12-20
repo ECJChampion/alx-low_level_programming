6f456430352279a2067c27eb52b0f2e102f638d9
Elekwachi Champion <117746523+ECJChampion@users.noreply.github.com>
2022-12-20 10:52:47 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,15 +1,11 @@
-void rev_string(char *s) {
-  // Find the length of the string
-  int len = 0;
-  while (s[len] != '\0') {
-    len++;
-  }
+#include <stdio.h>
+#include <string.h>
 
-  // Swap the characters at the beginning and end of the string,
-  // moving towards the middle of the string
-  for (int i = 0; i < len / 2; i++) {
-    char temp = s[i];
-    s[i] = s[len - i - 1];
-    s[len - i - 1] = temp;
-  }
+int main(void)
+{
+    char s[] = "hello";
+    rev_string(s);
+    printf("%s\n", s);  // outputs "olleh"
+
+    return 0;
 }
