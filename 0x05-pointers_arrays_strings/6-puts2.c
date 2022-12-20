8e4b18877b5a667881c2b8115ce1831a0c4b9ebb
Elekwachi Champion <117746523+ECJChampion@users.noreply.github.com>
2022-12-20 11:27:53 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 
-void print_every_other_char(const char* str) {
-  for (int i = 0; str[i] != '\0'; i += 2) {
+int main(void) {
+  char str[] = "abcdefghijklmnopqrstuvwxyz";
+
+  for (int i = 0; i < strlen(str); i += 2) {
     printf("%c\n", str[i]);
   }
-}
 
-int main(int argc, char* argv[]) {
-  print_every_other_char("abcdefg");
   return 0;
 }
