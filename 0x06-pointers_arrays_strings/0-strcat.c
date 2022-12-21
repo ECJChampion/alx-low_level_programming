7ff726cb5df6a570351fe7b968e3e3c4682c8a07
Elekwachi Champion <117746523+ECJChampion@users.noreply.github.com>
2022-12-21 12:47:47 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+char *_strcat(char *dest, char *src) {
+// Find the end of the dest string
+int dest_len = strlen(dest);
+// Append the src string to the end of dest
+strcpy(dest + dest_len, src);
+// Return a pointer to the resulting string
+return dest;
+}
+
+int main(void) {
+char dest[20] = "Hello, ";
+char *src = "world!";
+printf("%s\n", _strcat(dest, src)); // prints "Hello, world!"
+return 0;
+}
+
+
+
 
