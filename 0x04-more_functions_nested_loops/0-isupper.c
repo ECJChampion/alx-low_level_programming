b5d34585ad89eade295cd803e01f1c5d5894d945
Elekwachi Champion <117746523+ECJChampion@users.noreply.github.com>
2022-12-21 15:57:05 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main(void) {
-  int c = 'A';
-  if (_isupper(c)) {
-    printf("%c is an uppercase letter\n", c);
-  } else {
-    printf("%c is not an uppercase letter\n", c);
-  }
-  return 0;
-}
+isupper
