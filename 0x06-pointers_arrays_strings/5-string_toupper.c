89d7aa9058cf13bd8928035603452431173f5595
Elekwachi Champion <117746523+ECJChampion@users.noreply.github.com>
2022-12-21 12:57:28 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,11 @@
+char *string_toupper(char *str)
+{
+  for (int i = 0; str[i] != '\0'; i++)
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      str[i] = str[i] - 32;
+    }
+  }
+  return str;
+}
